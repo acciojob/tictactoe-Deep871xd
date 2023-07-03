b7a1d29256cef93aca53db6485a3181fba1a6430
Deep871xd <102525444+Deep871xd@users.noreply.github.com>
2023-07-03 16:31:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,48 +1,106 @@
-//your JS code here. If required.
-// Get player names and submit button
-const player1Input = document.getElementById('player-1');
-const player2Input = document.getElementById('player-2');
-const submitButton = document.getElementById('submit');
+const button = document.getElementById("submit");
+const message = document.getElementById("message");
+const boardGame = document.getElementById("board-game");
 
-// Get game board and message div
-const board = document.getElementById('board');
-const messageDiv = document.getElementById('message');
+const firstCont = document.querySelector(".first-cont");
+const secondCont = document.querySelector(".second-cont");
 
-// Player names and current turn
-let player1Name = '';
-let player2Name = '';
-let currentPlayer = '';
+const player1Sign = 'x';
+const player2Sign = 'o';
 
-// Add event listener to submit button
-submitButton.addEventListener('click', startGame);
+let winnerFound = false;
 
-// Function to start the game
-function startGame() {
-  // Get player names from input fields
-  player1Name = player1Input.value;
-  player2Name = player2Input.value;
+let currentPlayer;
 
-  // Set current player
-  currentPlayer = player1Name;
 
-  // Display whose turn is it
-  displayMessage(`${currentPlayer}, you're up!`);
+const winningIndexes = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
 
-  // Remove player input fields and submit button
-  player1Input.style.display = 'none';
-  player2Input.style.display = 'none';
-  submitButton.style.display = 'none';
-
-  // Add event listeners to board cells
-  const cells = Array.from(document.getElementsByClassName('cell'));
-  cells.forEach(cell => cell.addEventListener('click', cellClick));
+function addWhosTurn(){
+	message.innerHTML = `${currentPlayer}, you're up `;
 }
 
-// Function to handle cell click event
-function cellClick(event) {
-  const cell = event.target;
 
-  // Check if the cell is empty
-  if (!cell.innerHTML) {
-    // Set cell content to current player's symbol (X or O)
-    cell.innerHTML = currentPlayer === player
+function checkForWinner(){
+	let ret = false;
+	for(let x = 0; x < winningIndexes.length; x++){
+		const eachArr = winningIndexes[x];
+
+		const aa = document.getElementById(eachArr[0]);
+		const bb = document.getElementById(eachArr[1]);
+		const cc = document.getElementById(eachArr[2]);
+
+		const aaValue = aa.innerText;
+		const bbValue = bb.innerText;
+		const ccValue = cc.innerText;
+
+		if(aaValue !== "" && bbValue !== "" && ccValue !== ""){
+			if(aaValue == bbValue && bbValue == ccValue && ccValue == aaValue){
+				aa.classList.add("purple-bg");
+				bb.classList.add("purple-bg");
+				cc.classList.add("purple-bg");
+				ret = true;
+				break;
+			}
+		}
+				
+	}
+
+	return ret;
+}
+
+button.addEventListener("click", function(){
+	firstCont.classList.add("display-out");
+	secondCont.classList.add("display-in");
+
+
+	const player1 = document.getElementById("player1").value;
+	const player2 = document.getElementById("player2").value;
+
+	currentPlayer = player1;
+	
+	addWhosTurn();
+
+	for(let i = 1; i <= 9; i++){
+		let eachCell = document.createElement("div");
+		eachCell.classList.add("square");
+		eachCell.id = i;
+
+		eachCell.addEventListener("click", function(){
+			//console.log(eachCell);
+			if(winnerFound === false){
+
+			
+				if(currentPlayer == player1){
+					if(eachCell.innerText === ""){
+						eachCell.innerText = player1Sign;
+						currentPlayer = player2;
+					}
+				}else if(currentPlayer == player2){
+					if(eachCell.innerText === ""){ 
+						eachCell.innerText = player2Sign;
+						currentPlayer = player1;
+					}
+				}
+			}
+			addWhosTurn();
+
+			let received = checkForWinner();
+
+			if(received === true){
+				winnerFound = true;
+				
+				message.innerText = `${currentPlayer == player1? player2: player1} congratulations you won!`;
+			}
+		});
+
+		boardGame.appendChild(eachCell);
+
+		
+
+		
+		
+	}
+
+	
+	
+})
